3e844dd210b2db79d1d83356a057a5edb1a2c4a8
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-04-28 22:16:01 +0300

    Change author of project

--- slideshow.js
+++ slideshow.js
@@ -5,9 +5,15 @@ const time = 2500;
 //image list
 const img = document.querySelector(".bgimg-1");
 
-images.push("./img/background-1.png");
-images.push("./img/background-2.jpg");
-
+images.push(
+  "https://boutiquestoredesign.com/wp-content/uploads/2018/09/fashion-retail-womens-clothing-stores-design-ideas-layout-6.jpg"
+);
+images.push(
+  "https://images.pexels.com/photos/3768005/pexels-photo-3768005.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+);
+images.push(
+  "https://res.cloudinary.com/simpleview/image/upload/c_fill,w_840,h_400,q_auto:eco,f_auto,g_auto/crm/charleston/JordanLash110-c8b42aa15056a36_c8b42bed-5056-a36a-0684b396acf3ca59.jpg"
+);
 function changeImg() {
   img.style.backgroundImage = `url('${images[i]}')`;
   i = i < images.length - 1 ? i + 1 : 0;
